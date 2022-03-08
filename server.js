19e1de3a5578370a3e46f2084b69328d76aa3ddc
Manuel Remer <manuelremer@web.de>
2022-03-08 09:53:33 +0100

    nothing to commit?

--- server.js
+++ server.js
@@ -13,9 +13,14 @@ app.use(express.json());
 All your api endpoints should be prefixed with /api and be before the next ones
 If you have many endpoints, consider use Express Router for each set of endpoints
 */
+
 app.post("/api/v1/auth/register", async (req, res) => {
-  const newUser = await User.create(req.body);
-  res.status(200).json({ newUser });
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json({ newUser });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 });
 
 app.post("/api/v1/auth/login", (req, res) => {
