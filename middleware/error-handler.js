2c0c4bda0112df74758f1b81428573bb08a25820
Manuel Remer <manuelremer@web.de>
2022-03-09 16:53:22 +0100

    error handling: custom errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,33 @@
+const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(999).json({ msg: err.message });
+  console.log({ err }, `msg: ${err.message}`);
+
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong please try again later",
+  };
+
+  // duplicate value, but should be unique => if an email is already used
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  // invalid data entered => typo in email field
+  if (err.name === "TypeError") {
+    customError.msg = "invalid request, check your entered data";
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  // mongoose model validation failed
+  if (err.name === "ValidationError") {
+    console.log(Object.keys(err.errors));
+    customError.msg = `Please provide ${Object.keys(err.errors).join(", ")}`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 module.exports = errorHandlerMiddleware;
